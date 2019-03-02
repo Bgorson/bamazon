9685ca49ec9f16f23eeeb2b20d83b00683008658
BGorson <BGorson32@gmail.com>
2019-03-01 18:53:45 -0600

    added manager and dept totals

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,12 @@ const connection = mysql.createConnection({
     database: "bamazon_DB"
 });
 
+
+let chosenProductId;
+let amountChosen;
+let totalCost;
+let profit;
+
 function purchase() {
     // prompts user to enter the ID
     inquirer.prompt({
@@ -24,56 +30,86 @@ function purchase() {
         type: "input",
         message: "What product ID?"
     }).then(function (responseUserIDAnswer) {
+        chosenProductId = responseUserIDAnswer.product_id
         // after ID is selected, query is ran to get that ID's product name
         connection.query("SELECT product_name FROM products WHERE ?", {
-            item_id: responseUserIDAnswer.product_id
-        }, function (err, responseToIdQuery) {
-            if (err) throw err;
-            inquirer.prompt({
-                // user is then asked to select the amount
-                name: "amount",
-                type: "input",
-                message: "How much would you like?"
-            }).then(function (resAmount) {
-                // app checks to see how much of the item selected earlier is actually in stock
-                connection.query("SELECT stock_quantity FROM products Where ?", {
-                    item_id: responseUserIDAnswer.product_id
-                }, function (err, responseToAmountQuery) {
-                    if (err) throw err;
-                    // If there isn't enough, a console log is sent
-                    if (resAmount.amount > responseToAmountQuery[0].stock_quantity) {
-                        console.log("\n \n*********We don't have that amount in stock.*************\n")
-                        displayInventory();
-                    } else {
+            item_id: chosenProductId
+        })
+        //Selected and stored ID in variable
+ // user is then asked to select the amount
+        inquirer.prompt({
+            name: "amount",
+            type: "input",
+            message: "How much would you like?"
+        }).then(function (resAmount) {
+            amountChosen= resAmount.amount
+            // app checks to see how much of the item selected earlier is actually in stock
+            connection.query("SELECT stock_quantity FROM products Where ?", {
+                item_id: chosenProductId
+            }, function (err, responseToAmountQuery) {
+                if (err) throw err;
+                // If there isn't enough, a console log is sent
+                if (amountChosen > responseToAmountQuery[0].stock_quantity) {
+                    console.log("\n \n*********We don't have that amount in stock.*************\n")
+                    displayInventory();
+                } else {
+                    connection.query(
+                        //update the stock
+                        "UPDATE products SET ? WHERE ?",
+                        [{
+                                stock_quantity: responseToAmountQuery[0].stock_quantity - amountChosen
+                            },
+                            {
+                                item_id: chosenProductId
+                            }
+                        ],
+                        function (err, res) {
+                            console.log("Order placed!\n");
+                        })
+                    // If there is enough, a message is sent confirming the order, showing the price and updating the database with the withdrawn product
+                    connection.query("SELECT price FROM products Where ?", {
+                        item_id: chosenProductId
+                    }, function (err, responseToPriceQuery) {
+                        totalCost = (parseFloat(responseToPriceQuery[0].price) * resAmount.amount)
+
+                        //read current department sales 
+                    })
+
+                    connection.query(
+                        //determines dept Total
+                        "SELECT product_sales FROM products WHERE ?",
+                            {
+                                item_id: chosenProductId
+                            }
+                        ,
+                        function (err, deptSales) {
+                            profit = deptSales[0].product_sales + totalCost
                         connection.query(
+                            //Updates dept Total
                             "UPDATE products SET ? WHERE ?",
-                            [{
-                                    stock_quantity: responseToAmountQuery[0].stock_quantity - resAmount.amount
+                            [
+                                {
+                                product_sales: profit
                                 },
                                 {
-                                    product_name: responseToIdQuery[0].product_name
+                                item_id: chosenProductId  
                                 }
-                            ],
-                            function (err, res) {
-                                console.log("Order placed!\n");
+                            ], function (err, res) {
+                                console.log ("The total cost is: $"+ totalCost)
                             })
-
-                        // If there is enough, a message is sent confirming the order, showing the price and updating the database with the withdrawn product
-                        connection.query("SELECT price FROM products Where ?", {
-                            item_id: responseUserIDAnswer.product_id
-                        }, function (err, responseToPriceQuery) {
-                            console.log("The cost of the purchase is $" + (parseFloat(responseToPriceQuery[0].price) * resAmount.amount))
-                            connection.end();
-                        })
-                    }
+                            
+                    connection.end();   
                 })
+                }
 
             })
+      
+        })
 
-        });
 
-    })
+    });
 }
+
 
 function displayInventory() {
     //all items are shown and formated
--- bamazonDept.sql
+++ bamazonDept.sql
@@ -1,1 +1,13 @@
-Enter MYSQL Data for making a dept table
+USE bamazon_DB;
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(50) NOT NULL,
+  over_head_costs DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ('produce', 300);
+
+SELECT * FROM departments;
